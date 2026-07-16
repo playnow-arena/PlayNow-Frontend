e7dc6db5c6ae862dc0b8fc7a4385c8cd17448d9d
nithish-ghub <nknithish10gmail.com>
2026-07-16 23:55:19 +0530

    Remove mock data and use backend APIs

--- src/data/mockData.js
+++
@@ -1,8 +0,0 @@
-export const sportsList = [
-  { id: '1', name: 'Badminton', icon: 'badminton', color: 'bg-green-500' },
-  { id: '2', name: 'Football Turf', icon: 'football', color: 'bg-blue-500' },
-  { id: '3', name: 'Cricket', icon: 'cricket', color: 'bg-yellow-500' },
-  { id: '4', name: 'Tennis', icon: 'tennis', color: 'bg-orange-500' },
-  { id: '5', name: 'Basketball', icon: 'basketball', color: 'bg-red-500' },
-  { id: '6', name: 'Pickleball', icon: 'pickleball', color: 'bg-purple-500' },
-];
